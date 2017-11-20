9ea10f212e9be8d36c4fb4a6a753233e0980dfc5
wohlert <wohlert.jesper@gmail.com>
2017-11-20 23:07:27 +0100

    add google cloud install script

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
-# Bayesian deep learning
+# Bayesian deep transfer learning
 
-This branch contains a modified version of the code that puts
-the data into a more organised view.
+This repository implements Bayes by Backprop [(Blundell 2015)](https://arxiv.org/abs/1505.05424)
+along with some neat methods to increase performance. The goal of the
+project is to understand how learning a posterior distribution
+over some task can help in transfering to a new domain by using
+the learnt posterior as a prior.
+
+## Implemented methods
+
+* Bayes by Backprop MLP [(Blundell 2015)](https://arxiv.org/abs/1505.05424)
+* Normalizing flows [(Rezende 2015)](https://arxiv.org/abs/1505.05770)
+* Bayes by Backprop Convolutional network
+
+## How to run on Google Compute Engine
+
+After creating a project go to **VM instances** and create a new instance.
+For machine type choose atleast 2 CPU cores and 6 GB of memory and choose
+the number of GPUs under advanced settings - 1 K80 will usually do.
+
+For boot disk choose Ubuntu 16.04 LTS and increase the disk size to atleast 30 GB.
+If you do not have any project wide SSH-keys, you will need to add them inside the
+management tab by copy-pasting your public key.
+
+When the machine is up, it will give you an external IP-address from which you
+can SSH into, for example:
+
+```
+ssh -i ~/.ssh/id_rsa 'your-username-for-the-key'@'external-ip'
+```
+
+When inside the machine, after cloning the repository you will need to run
+the install script to get CUDA 8 and PyTorch.
+
+```
+chmod +x gcloud.sh
+sudo sh ./gcloud.sh
+```
+
+This should install everything and you can run the `main.py` file.
---
+++ gcloud.sh
@@ -0,0 +1,82 @@
+# This script is designed to work with ubuntu 16.04 LTS
+# with the latest Pytorch with CUDA 8 support
+#
+# Based on https://gist.github.com/motiur/2e0cd3d35bc1c42b6e5d6046b65be6f4
+##########################################################################
+#This is used to install CUDA 8 driver for Tesla K80
+##########################################################################
+
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda-8-0; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  apt-get update
+  apt-get install cuda-8-0 -y
+fi
+
+#########################################################################
+
+#############################################################################
+#Updating the system
+#############################################################################
+
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install tmux build-essential gcc g++ make binutils
+sudo apt-get --assume-yes install software-properties-common
+
+#########################################################################################################################
+#Installing anaconda with the required packages
+#########################################################################################################################
+
+wget "https://repo.continuum.io/archive/Anaconda3-4.3.0-Linux-x86_64.sh" -O "Anaconda3-4.3.0-Linux-x86_64.sh"
+bash Anaconda3-4.3.0-Linux-x86_64.sh -b
+echo "export PATH=\"$HOME/anaconda3/bin:\$PATH\"" >> ~/.bashrc
+export PATH="$HOME/anaconda3/bin:$PATH"
+conda install -y bcolz
+conda upgrade -y --all
+
+#########################################################################################################################
+#Installing Jupyter notebook
+#########################################################################################################################
+
+# configure jupyter and prompt for password
+jupyter notebook --generate-config
+jupass=`python -c "from notebook.auth import passwd; print(passwd())"`
+echo "c.NotebookApp.password = u'"$jupass"'" >> $HOME/.jupyter/jupyter_notebook_config.py
+echo "c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
+echo "\"jupyter notebook\" will start Jupyter on port 8888"
+echo "If you get an error instead, try restarting your session so your $PATH is updated"
+
+#########################################################################################################################
+#Installing google compute engine package, unzip package, and gensim package
+#########################################################################################################################
+
+sudo apt-get install unzip
+pip install --upgrade gensim
+pip install google-compute-engine
+
+#########################################################################################################################
+#Installing PyTorch from source
+#########################################################################################################################
+
+export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../" # [anaconda root directory]
+
+# Install basic dependencies
+conda install -y numpy pyyaml mkl setuptools cmake cffi
+
+# Add LAPACK support for the GPU
+conda install -y -c soumith magma-cuda80 # or magma-cuda75 if CUDA 7.5
+
+# Download master branch of PyTorch
+git clone --recursive https://github.com/pytorch/pytorch
+
+# Install
+cd pytorch
+python setup.py install
+
+cd ..
