d01822faea0d4dae685b1e89766cec1ed19c5e8c
wohlert <wohlert.jesper@gmail.com>
2017-12-04 14:30:17 +0100

    Add logfile extraction

---
+++ extract.py
@@ -0,0 +1,40 @@
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+plt.style.use("seaborn")
+import re
+import numpy as np
+
+def load_data(basename, intervals):
+    files = [open("{}{}/logfile.txt".format(basename, i)).read() for i in intervals]
+    acc = [list(map(lambda x: x.split(" ")[-1], re.findall(r"(acc: \d.\d+)", f))) for f in files]
+    if basename is "domain":
+        print(acc)
+    train = list(map(lambda x: x[-3], acc))
+    valid = list(map(lambda x: x[-2], acc))
+    MAP = list(map(lambda x: x[-1], acc))
+    return np.array(train).astype(np.float32), np.array(valid).astype(np.float32), np.array(MAP).astype(np.float32)
+
+i = [0.05, 0.1, 0.2, 0.3, 0.5, 1]
+f = plt.figure(figsize=(10, 8))
+
+train, valid, MAP = load_data("transfer_cnn_domain", [0.05, 0.1, 0.2, 0.3, 0.5, 0.5])
+
+#plt.plot(i, train, label=r"Train, prior: $q(w \mid \theta)$", color="#9c209b")
+plt.plot(i, valid, "--", label=r"Validation, prior: $q(w \mid \theta)$", color="#d534d3")
+plt.plot(i, MAP, "--", label=r"MAP, prior: $q(w \mid \theta)$", color="#e273e1")
+
+train, valid, MAP = load_data("cnn_domain", i)
+
+#plt.plot(i, train, label=r"Train, prior: $\mathcal{U}(a, b)$", color="#209c22")
+plt.plot(i, valid, "--", label=r"Validation, prior: $\mathcal{U}(a, b)$", color="#34d536")
+plt.plot(i, MAP, "--", label=r"MAP, prior: $\mathcal{U}(a, b)$", color="#73e275")
+
+plt.xlabel("Size of transfer dataset")
+plt.ylabel("Accuracy")
+plt.xticks(i, map(lambda x: "{}%".format(int(x*100)), i))
+f.suptitle("Accuracy after training for 50 epochs")
+plt.legend()
+
+plt.savefig("cnn_train_acc.pdf")
+
