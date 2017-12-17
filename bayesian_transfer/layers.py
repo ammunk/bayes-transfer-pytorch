a609b382698701a44234986e4679f73f1267bdc4
wohlert <wohlert.jesper@gmail.com>
2017-12-17 13:55:32 +0100

    New dataloaders

--- bayesian_transfer/layers.py
+++ bayesian_transfer/layers.py
@@ -5,7 +5,6 @@ from torch.nn import Parameter
 import torch.nn.functional as F
 
 from .distributions import Normal, distribution_selector
-from .normflow import NormalizingFlows
 
 
 class BBBLinearFactorial(nn.Module):
@@ -115,3 +114,53 @@ class GaussianVariationalInference(nn.Module):
         loss = -ll
 
         return loss
+
+
+class NormalizingFlows(nn.Module):
+    def __init__(self, n, features):
+        super(NormalizingFlows, self).__init__()
+        flows = [PlanarNormalizingFlow(features) for _ in range(n)]
+        self.flows = nn.ModuleList(flows)
+
+    def forward(self, z):
+        log_dets = []
+
+        for flow in self.flows:
+            z, log_det_jacobian = flow(z)
+            log_dets.append(log_det_jacobian)
+
+        return z, log_dets
+
+
+class PlanarNormalizingFlow(nn.Module):
+    """
+    Based on Normalizing Flow implementation from Parmesan
+    https://github.com/casperkaae/parmesan
+    """
+    def __init__(self, features):
+        super(PlanarNormalizingFlow, self).__init__()
+        self.u = Parameter(torch.randn(features))
+        self.w = Parameter(torch.randn(features))
+        self.b = Parameter(torch.ones(1))
+
+    def forward(self, z):
+        # Create uhat such that it is parallel to w
+        uw = torch.dot(self.u, self.w)
+        muw = -1 + F.softplus(uw)
+        uhat = self.u + (muw - uw) * torch.transpose(self.w, 0, -1) / torch.sum(self.w ** 2)
+
+        # Equation 21 - Transform z
+        zwb = torch.mv(z, self.w) + self.b
+
+        f_z = z + (uhat.view(1, -1) * F.tanh(zwb).view(-1, 1))
+
+        # Compute the Jacobian using the fact that
+        # tanh(x) dx = 1 - tanh(x)**2
+        psi = (1 - F.tanh(zwb)**2).view(-1, 1) * self.w.view(1, -1)
+        psi_u = torch.mv(psi, uhat)
+
+        # Return the transformed output along
+        # with log determninant of J
+        logdet_jacobian = torch.log(torch.abs(1 + psi_u) + 1e-8)
+
+        return f_z, logdet_jacobian
