efe35577eecc8bc85d488fcb1c2b0db3aa9e6a26
wohlert <wohlert.jesper@gmail.com>
2017-12-17 11:23:48 +0000

    Working GPU accelaration

--- experiment.py
+++ experiment.py
@@ -2,6 +2,7 @@ import math
 import os
 import gc
 import pickle
+import numpy as np
 
 import torch
 from torch.autograd import Variable
@@ -14,7 +15,6 @@ from bayesian_transfer.model import BBBMLP
 from bayesian_transfer.datasets import LimitedMNIST
 
 cuda = torch.cuda.is_available()
-
 
 def get_model(num_output, num_hidden=100, num_layers=2, num_flows=0, pretrained=None):
     model = BBBMLP(in_features=784, num_class=num_output, num_hidden=num_hidden, num_layers=num_layers, nflows=num_flows)
@@ -43,27 +43,24 @@ def get_data(digits, fraction):
 
 ex = Experiment("Bayesian Deep Transfer Learning")
 
-
 @ex.named_config
 def blundell():
-    experiment_name="results/blundell"
+    experiment_name="results/blundell2"
     num_layers = 2
     num_hidden = 400
     num_samples = 10
     num_epochs = 51
     beta_type = "Blundell"
-
 
 @ex.named_config
 def normflow():
     experiment_name = "results/normflow"
-    num_flows = 16
     num_layers = 2
     num_hidden = 400
     num_samples = 10
+    num_flows = 16
     num_epochs = 51
     beta_type = "Blundell"
-
 
 @ex.named_config
 def domain_a():
@@ -72,47 +69,65 @@ def domain_a():
     num_layers = 2
     num_hidden = 400
     num_samples = 10
-    num_epochs = 51
+    num_epochs = 601
     beta_type = "Blundell"
 
-
 @ex.named_config
-def domain_b():
-    digits = [5,6,7,8,9]
+def domain_b(fraction):
+    experiment_name = "results/domain_b_{}".format(fraction)
     num_layers = 2
     num_hidden = 400
     num_samples = 10
-    num_epochs = 51
+    num_epochs = 601
     beta_type = "Blundell"
 
-
 @ex.named_config
-def transfer():
+def transfer(fraction):
+    experiment_name = "results/domain_b_{}".format(fraction)
     pretrained = "results/domain_a"
-    digits = [5,6,7,8,9]
     num_layers = 2
     num_hidden = 400
     num_samples = 10
-    num_epochs = 51
+    num_epochs = 601
     beta_type = "Blundell"
 
-
 @ex.named_config
-def beta():
+def beta_standard():
+    experiment_name = "results/beta_standard"
     num_layers = 2
     num_hidden = 400
     num_samples = 10
     num_epochs = 51
+    beta_type = "Standard"
+
+@ex.named_config
+def beta_soenderby():
+    experiment_name = "results/beta_soenderby"
+    num_layers = 2
+    num_hidden = 400
+    num_samples = 10
+    num_epochs = 51
+    beta_type = "Soenderby"
+
+@ex.named_config
+def beta_none():
+    experiment_name = "results/beta_none"
+    num_layers = 2
+    num_hidden = 400
+    num_samples = 10
+    num_epochs = 51
+    beta_type = "None"
+
 
 
 @ex.automain
-def main(experiment_name, digits=list(range(10)), fraction=1.0, pretrained=None, num_samples=10, num_flows=0, beta_type="Blundell",
+def main(experiment_name, digits=list(range(10)), fraction=1.0, pretrained=None, num_samples=16, num_flows=0, beta_type="Blundell",
          num_layers=2, num_hidden=100, num_epochs=51):
     if not os.path.exists(experiment_name): os.makedirs(experiment_name)
     logfile = os.path.join(experiment_name, 'diagnostics.txt')
     with open(logfile, "w") as fh:
         fh.write("")
-        
+
     loader_train, loader_val = get_data(digits, fraction)
 
     model = get_model(len(digits), num_hidden=num_hidden, num_layers=num_layers, num_flows=num_flows, pretrained=pretrained)
@@ -120,26 +135,33 @@ def main(experiment_name, digits=list(range(10)), fraction=1.0, pretrained=None,
     vi = GaussianVariationalInference(torch.nn.CrossEntropyLoss())
     optimizer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-3)
 
+    if cuda:
+        model.cuda()
+
     def run_epoch(loader, epoch, is_training=False):
         m = math.ceil(len(loader.dataset) / loader.batch_size)
 
         accuracies = []
         losses = []
 
-        for i, (data, labels) in enumerate(tqdm(loader)):
+        for i, (data, labels) in tqdm(enumerate(loader)):
             # Repeat samples
             x = data.view(-1, 784).repeat(num_samples, 1)
             y = labels.repeat(num_samples, 0)
 
+            if cuda:
+                x = x.cuda()
+                y = y.cuda()
+
             if beta_type == "Blundell":
                 beta = 2 ** (m - (i + 1)) / (2 ** m - 1)
             elif beta_type == "Soenderby":
-                # Deterministic warmup
-                beta = min(epoch / (num_epochs//2), 1)
+                beta = min(epoch / 100, 1)
             elif beta_type == "Standard":
                 beta = 1 / m
             else:
                 beta = 0
+
 
             logits, kl = model.probforward(Variable(x))
             loss = vi(logits, Variable(y), kl, beta)
@@ -161,6 +183,7 @@ def main(experiment_name, digits=list(range(10)), fraction=1.0, pretrained=None,
         return diagnostics
 
     for epoch in range(num_epochs):
+        print("Epoch {}/{}".format(epoch, num_epochs))
         diagnostics_train = run_epoch(loader_train, epoch, is_training=True)
         diagnostics_val = run_epoch(loader_val, epoch)
 
